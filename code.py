b592d50e02c23b0b927059c0e40434976cfba23d
Andrea Álvarez Sánchez <andreaalvarezsanchez@MacBook-Air-de-Andrea.local>
2025-05-05 18:41:33 +0200

    dia 2

--- code.py
+++ code.py
@@ -1,1 +1,12 @@
 import pandas as pd
+import os
+
+print(os.getcwd())
+
+df = pd.read_csv('loan_dataset.csv')
+
+print('\n')
+print(df['loan_status'].value_counts())
+
+print('\n')
+print(df.describe())
